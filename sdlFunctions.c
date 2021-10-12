18aa3ca5a330d274584dd8c46684e4b362ee009c
toadster172 <toadster172@gmail.com>
2021-10-12 11:53:30 -0500

    Corrected date

--- sdlFunctions.c
+++ sdlFunctions.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020, 2p21 Alice Shelton <AliceAShelton@gmail.com>
+/* Copyright (C) 2020, 2021 Alice Shelton <AliceAShelton@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
